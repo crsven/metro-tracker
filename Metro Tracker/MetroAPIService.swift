5a5a70c4e97875172453814e6f8d57df24263531
Chris Svenningsen <chris@carbonfive.com>
2015-02-17 11:43:47 -0800

    Add basic test for BusLine.

---
+++ Metro Tracker/MetroAPIService.swift
@@ -0,0 +1,9 @@
+//
+//  MetroAPIService.swift
+//  Metro Tracker
+//
+//  Created by Chris Svenningsen on 2/17/15.
+//  Copyright (c) 2015 Chris Svenningsen. All rights reserved.
+//
+
+import Foundation
---
+++ Metro TrackerTests/BusLineTests.swift
@@ -0,0 +1,29 @@
+//
+//  BusLineTests.swift
+//  Metro Tracker
+//
+//  Created by Chris Svenningsen on 2/17/15.
+//  Copyright (c) 2015 Chris Svenningsen. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+class BusLineTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testCanInitWithName() {
+        let newName = "A Great Bus Line"
+        let busLine = BusLine(name: newName)
+
+        XCTAssertEqual(busLine.name, newName)
+    }
+}
