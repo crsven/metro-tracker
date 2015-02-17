6dcfb5e60ca6a5a5902b48f20d9c52e39d4441e1
Chris Svenningsen <chris@carbonfive.com>
2015-02-17 11:22:50 -0800

    Add hardcoded bus line list.

---
+++ Metro Tracker/BusLine.swift
@@ -0,0 +1,13 @@
+//
+//  BusLine.swift
+//  Metro Tracker
+//
+//  Created by Chris Svenningsen on 2/17/15.
+//  Copyright (c) 2015 Chris Svenningsen. All rights reserved.
+//
+
+import Foundation
+
+struct BusLine {
+    let name : String
+}
---
+++ Metro Tracker/BusLineTableViewController.swift
@@ -0,0 +1,106 @@
+//
+//  BusLineTableViewController.swift
+//  Metro Tracker
+//
+//  Created by Chris Svenningsen on 2/17/15.
+//  Copyright (c) 2015 Chris Svenningsen. All rights reserved.
+//
+
+import UIKit
+
+class BusLineTableViewController: UITableViewController {
+    var busLines = [BusLine]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.busLines = [
+            BusLine(name: "720 - East"),
+            BusLine(name: "720 - West"),
+            BusLine(name: "704 - East"),
+            BusLine(name: "704 - West"),
+            BusLine(name: "700 - East"),
+            BusLine(name: "700 - West")
+        ]
+
+        self.tableView.reloadData()
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.busLines.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
+
+        let busLine = self.busLines[indexPath.row]
+        cell.textLabel!.text = busLine.name
+        cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
+
+        return cell
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
