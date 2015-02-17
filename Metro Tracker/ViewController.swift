6f5a0d0bbfe8e06104e7950097f27fd4e55a706a
Chris Svenningsen <chris@carbonfive.com>
2015-02-17 10:50:33 -0800

    Initial commit.

---
+++ Metro Tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Metro Tracker
+//
+//  Created by Chris Svenningsen on 2/17/15.
+//  Copyright (c) 2015 Chris Svenningsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
